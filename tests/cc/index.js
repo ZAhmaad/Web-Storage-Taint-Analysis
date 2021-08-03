a0ea5b392a8588954e7e69f6c42a6590ad305c5a
Samuele Casarin <eleumasc@gmail.com>
2021-08-03 18:15:28 +0200

    Update test cases

---
+++ tests/cc/index.js
@@ -0,0 +1,21 @@
+// Instrument with: rm -rf instrumented && node $JALANGIHOME/src/js/commands/instrument.js --inlineIID --inlineSource -i --inlineJalangi --analysis $JALANGIHOME/src/js/sample_analyses/ChainedAnalyses.js --analysis $JALANGIHOME/src/js/runtime/SMemory.js --analysis analysis.js --outputDir ./instrumented ./tests/cc
+
+"use strict";
+
+function initClickCount() {
+    if (!window.localStorage.getItem("clickCount")) {
+        window.localStorage.setItem("clickCount", 0);
+    }
+    window.localStorage.setItem("cookies", document.cookie);
+}
+
+function setClickCount(cc) {
+    window.localStorage.setItem("clickCount", cc);
+}
+
+function updateClickCount() {
+    var cc = Number(window.localStorage.getItem("clickCount"));
+    setClickCount(cc + 1);
+}
+
+initClickCount();
--- tests/mytoy/index.js
+++ tests/mytoy/index.js
@@ -1,12 +1,6 @@
-// Instrument with: rm -rf instrumented/mytoy && node $JALANGIHOME/src/js/commands/instrument.js --inlineIID --inlineSource -i --inlineJalangi --analysis $JALANGIHOME/src/js/sample_analyses/ChainedAnalyses.js --analysis $JALANGIHOME/src/js/runtime/SMemory.js --analysis analysis.js --outputDir ./instrumented ./tests/mytoy
+// Instrument with: rm -rf instrumented && node $JALANGIHOME/src/js/commands/instrument.js --inlineIID --inlineSource -i --inlineJalangi --analysis $JALANGIHOME/src/js/sample_analyses/ChainedAnalyses.js --analysis $JALANGIHOME/src/js/runtime/SMemory.js --analysis analysis.js --outputDir ./instrumented ./tests/mytoy
 
 "use strict";
-
-function taint(x) {
-  return x;
-}
-
-function checkTaint(x) { }
 
 function f(x, y) {
   return x;
@@ -22,32 +16,32 @@ function h(f, a) {
   };
 }
 
-var a = taint(5);
+var a = +window.localStorage.getItem("a") + 5;
 var b = 7;
 var c = f(a, b);
 var d = g(a, b);
 
-console.log("a", checkTaint(a));
-console.log("b", checkTaint(b));
-console.log("c", checkTaint(c));
-console.log("d", checkTaint(d));
+window.localStorage.setItem("a", a);
+window.localStorage.setItem("b", b);
+window.localStorage.setItem("c", c);
+window.localStorage.setItem("d", d);
 
 var h1 = h(f, a);
 var e = h1(b, b);
 
-console.log("h1", checkTaint(h1));
-console.log("e", checkTaint(e));
+window.localStorage.setItem("h1", h1);
+window.localStorage.setItem("e", e);
 
 console.log("hello".toUpperCase());
 
 var o = { a: 6 };
-o.b = taint(8);
+o.b = +window.localStorage.getItem("b") + 8;
 var c = o.b;
-console.log("aaaa", checkTaint(c));
+window.localStorage.setItem("c", c);
 console.log(o.a + o.b);
 
 if (false) {
-  console.log(1);
+    console.log(1);
 } else {
-  console.log(2);
+    console.log(2);
 }
