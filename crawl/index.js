3b32a6e1e4db6f86a98dc6e0abe6c10880f5b6ba
Samuele Casarin <eleumasc@gmail.com>
2021-11-29 09:18:04 +0100

    Add crawl

---
+++ crawl/index.js
@@ -0,0 +1,84 @@
+(async () => {
+
+    require("dotenv").config();
+    if (!process.env.PROXY_SPKI_FINGERPRINT) {
+        throw new Error("PROXY_SPKI_FINGERPRINT is not defined in environment.");
+    }
+
+    const fs = require("fs");
+    const fsPromises = fs.promises;
+    const pptr = require("puppeteer");
+
+    const browser = await pptr.launch({
+        args: [
+            "--proxy-server=https://127.0.0.1:8000",
+            "--ignore-certificate-errors-spki-list=" + process.env.PROXY_SPKI_FINGERPRINT,
+            "--disable-web-security",
+        ],
+    });
+
+    const startFrom = 0;
+
+    const sites = (await fsPromises.readFile("/home/eleumasc/tranco_9892.csv")).toString().split("\n").map(line => line.split(",")[1]);
+
+    for (let i in sites) {
+        if (i < startFrom) continue;
+        const domain = sites[i];
+        const url = "https://" + domain;
+        console.log(`[${i}] ${url}`);
+        try {
+            await fsPromises.appendFile("out.txt", JSON.stringify(await crawl(browser, url, domain)) + "\n");
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    await browser.close();
+})();
+
+async function crawl(browser, url, domain) {
+    const page = await browser.newPage();
+
+    const grepResult = [];
+
+    try {
+        page.on("request", async request => {
+            request.continue();
+        })
+
+        page.on("response", async response => {
+            if (response.request().resourceType() === "script" &&
+                response.headers()["content-type"] &&
+                response.headers()["content-type"].includes("javascript")) {
+                    const js = await response.text();
+                    const grepPartResult = grepMagicWords(js);
+                    grepResult.push([response.request().url(), grepPartResult]);
+            }
+        });
+
+        await page.setRequestInterception(true);
+
+        try {
+            await page.goto(url, {waitUntil: "load", timeout: 60000});
+            await new Promise(resolve => { setTimeout(resolve, 15000); });
+        } catch (e) { console.error(e); }
+
+        const flows = await Promise.race([
+            page.evaluate(() => J$.FLOWS),
+            new Promise((_, reject) => { setTimeout(() => { reject(); }, 5000); })
+        ]);
+
+        return {url: url, grepResult: grepResult, flows: flows};
+    } finally {
+        await page.close();
+    }
+
+    function grepMagicWords(js) {
+        var re = /(?:\'|\")(?:g|s)etItem(?:\'|\")/g, match, result = [];
+        while (match = re.exec(js)) {
+            result.push(js.substring(match.index - 100, match.index + 100));
+        }
+        return result;
+    }
+
+}
