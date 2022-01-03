8018bd645b9f6113f461249189fc78f04ec7b49b
Samuele Casarin <eleumasc@gmail.com>
2022-01-03 02:03:07 +0100

    Add HTTP vs HTTPS

---
+++ empirical/lib/http.js
@@ -0,0 +1,15 @@
+"use strict";
+
+import { labelsRelatedTo } from "./labels.js";
+import { countFlows } from "./report.js";
+
+function countNetworkFlowsWithHttp(data, secure) {
+    return countFlows(data, flow =>
+        labelsRelatedTo(flow, "network")
+            .some(lbl => lbl.srcUrl.protocol === (secure ? "https:" : "http:"))
+    );
+}
+
+export {
+    countNetworkFlowsWithHttp
+};
